5a214223a064c4b9f5658cc88eefc2512f66c9ac
zingale <zingale>
2012-08-27 17:10:55 +0000

    basic structure working

--- pyro.py
+++ pyro.py
@@ -3,6 +3,7 @@
 import sys
 import getopt
 import numpy
+import pylab
 
 from util import runparams
 
@@ -40,15 +41,57 @@ except IndexError:
     print usage
     sys.exit(2)
 
-# actually import the solver-specific stuff
+try: problemName = next[1]
+except IndexError:
+    print 'ERROR: problem name not specified on command line'
+    print 'USAGE: pyro.py [options] <solver name> <problem name> <input file>'
+    sys.exit(2)
+
+
+# actually import the solver-specific stuff under the 'solver' namespace
 exec 'import ' + solverName + ' as solver'
+
+
+#-----------------------------------------------------------------------------
+# runtime parameters
+#-----------------------------------------------------------------------------
 
 # parameter defaults
 runparams.LoadParams(solverName + "/_defaults")
 
 # problem-specific runtime parameters
+runparams.LoadParams(solverName + "/problems/_" + problemName + ".defaults")
+
+runparams.PrintParamFile()
 
 
-# initialize the grid
+
+#-----------------------------------------------------------------------------
+# initialization
+#-----------------------------------------------------------------------------
+
+# initialize the grid structure
 myGrid, myData = solver.initialize()
     
+
+# initialize the data
+exec 'from ' + solverName + '.problems import *'
+
+exec problemName + '.fillPatch(myData)'
+
+
+# debugging -- look at the data
+dens = myData.getVarPtr("density")
+myg = myData.grid
+pylab.imshow(numpy.transpose(dens), interpolation="nearest", origin="lower", 
+             extent=[myg.xmin, myg.xmax, myg.ymin, myg.ymax])
+
+pylab.draw()
+
+pylab.show()
+
+
+#-----------------------------------------------------------------------------
+# evolve
+#-----------------------------------------------------------------------------
+
