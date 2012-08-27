1f7221be1fbe299b3083c8e6ba6c1c05f8f1c658
zingale <zingale>
2012-08-27 17:02:55 +0000

    more progress

---
+++ advection/problems/smooth.py
@@ -0,0 +1,53 @@
+"""
+  ---------------------------------------------------------------------
+  Copyright (C) 2003, 2004  Michael Zingale
+
+  This software is distributed under the terms of the GNU General
+  Public License, version 2.  See COPYING for details.  It is covered
+  by no warantee whatsoever, explicit or implied.
+  ---------------------------------------------------------------------
+"""
+
+
+import sys
+from util import runparams
+import mesh.patch as patch
+import numpy
+
+def fillPatch(myPatch):
+    """ initialize the smooth advection problem """
+
+    print "initializing the smooth advection problem..."
+
+    # make sure that we are passed a valid patch object
+    if not isinstance(myPatch, patch.ccData2d):
+        print "ERROR: patch invalid in smooth.py"
+        print myPatch.__class__
+        sys.exit()
+
+    dens = myPatch.getVarPtr("density")
+
+    xmin = runparams.getParam("mesh.xmin")
+    xmax = runparams.getParam("mesh.xmax")
+
+    ymin = runparams.getParam("mesh.ymin")
+    ymax = runparams.getParam("mesh.ymax")
+
+    xctr = 0.5*(xmin + xmax)
+    yctr = 0.5*(ymin + ymax)
+    
+    i = myPatch.grid.ilo
+    while i <= myPatch.grid.ihi:
+
+        j = myPatch.grid.jlo
+        while j <= myPatch.grid.jhi:
+
+            dens[i,j] = numpy.exp(-60.0*((myPatch.grid.x[i]-xctr)**2 + \
+                                         (myPatch.grid.y[j]-yctr)**2))
+                    
+            j += 1
+        i += 1
+    
+
+    
+                             
