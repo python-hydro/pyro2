17555cd048432bfd858e5d95e5f6b612bc2005b0
zingale <zingale>
2012-09-26 14:33:55 +0000

    make tests

--- mk.sh
+++ mk.sh
@@ -3,12 +3,23 @@
 # reconstruction library
 echo "making reconstruction_f.so..."
 cd mesh
-make
+make >> /dev/null
+err=$?
+if [ $err -ne 0 ]; then
+    echo "Error making reconstruction_f.so"
+    exit $err
+fi
 cd ..
 
+# compressible interface stuff
 echo "making interface_f.so..."
 cd compressible
-make
+make >> /dev/null
+err=$?
+if [ $err -ne 0 ]; then
+    echo "Error making interface_f.so"
+    exit $err
+fi
 cd ..
 
 echo "done"
