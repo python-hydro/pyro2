3469e789f060dd803e46249093ff00972345f195
zingale <zingale>
2012-09-26 18:20:47 +0000

    pretty I/O to screen

---
+++ util/msg.py
@@ -0,0 +1,30 @@
+import sys
+
+# inspiration from                                                                          
+# http://stackoverflow.com/questions/287871/print-in-terminal-with-colors-using-python      
+# which in-turn cites the blender build scripts                                             
+class termColors:
+    WARNING = '\033[33m'
+    SUCCESS = '\033[32m'
+    FAIL = '\033[31m'
+    BOLD = '\033[1m'
+    ENDC = '\033[0m'
+
+
+def fail(str):
+    new_str = termColors.FAIL + str + termColors.ENDC
+    print new_str
+    sys.exit()
+
+def warning(str):
+    new_str = termColors.WARNING + str + termColors.ENDC
+    print new_str
+
+def success(str):
+    new_str = termColors.SUCCESS + str + termColors.ENDC
+    print new_str
+
+def bold(str):
+    new_str = termColors.BOLD + str + termColors.ENDC
+    print new_str
+
