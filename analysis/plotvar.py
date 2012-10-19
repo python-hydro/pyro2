7208d0499f4c041cec74b59e971847a9b9937b0d
zingale <zingale>
2012-10-19 18:25:52 +0000

    plot a single variable

---
+++ analysis/plotvar.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+import numpy
+import pylab
+import sys
+import mesh.patch as patch
+
+# plot an output file using the solver's dovis script
+
+def makeplot(myd, variable):
+
+    pylab.figure(num=1, figsize=(6.5,5.25), dpi=100, facecolor='w')
+
+    var = myd.getVarPtr(variable)
+    myg = myd.grid
+
+    img = pylab.imshow(numpy.transpose(var[myg.ilo:myg.ihi+1,myg.jlo:myg.jhi+1]),
+                       interpolation="nearest", origin="lower",
+                       extent=[myg.xmin, myg.xmax, myg.ymin, myg.ymax])
+
+    #pylab.axis("off")
+    pylab.savefig("plot.png")
+    pylab.show()
+
+
+if __name__== "__main__":
+
+    print sys.argv
+
+    file = sys.argv[1]
+    variable = sys.argv[2]
+
+    myg, myd = patch.read(file)
+
+    makeplot(myd, variable)
+
+
+
+
