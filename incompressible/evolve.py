76eeacf9cd6badb3c064e516424d29eb4e9238fa
Michael Zingale <zingale@bender.astro.sunysb.edu>
2012-11-01 15:40:29 -0400

    more progress

---
+++ incompressible/evolve.py
@@ -0,0 +1,144 @@
+from util import runparams
+
+
+def evolve(myData, dt):
+    """ evolve the incompressible equations through one timestep """
+
+    
+    u = myData.getVarPtr("x-velocity")
+    v = myData.getVarPtr("x-velocity")
+
+    gradp_x = myData.getVarPtr("gradp_y")
+    gradp_y = myData.getVarPtr("gradp_x")
+
+    myg = myData.grid
+
+    dtdx = dt/myg.dx
+    dtdy = dt/myg.dy
+
+    #-------------------------------------------------------------------------
+    # create the limited slopes of u and v
+    #-------------------------------------------------------------------------
+
+
+    #-------------------------------------------------------------------------
+    # create the transverse velocities to be used in the advection
+    #-------------------------------------------------------------------------
+    
+    utrans, vtrans = \
+        incomp_interface_f.trans_vels(myg.qx, myg.qy, myg.ng, 
+                                      myg.dx, myg.dy, dt,
+                                      u, v,
+                                      ldelta_u, ldelta_v)
+
+    
+
+
+    #-------------------------------------------------------------------------
+    # get the advective velocities
+    #-------------------------------------------------------------------------
+    
+    """
+    the advective velocities are the normal velocity through each cell
+    interface, and are defined on the cell edges, in a MAC type
+    staggered form
+
+                   n+1/2 
+                  v 
+                   i,j+1/2 
+              +------+------+
+              |             | 
+      n+1/2   |             |   n+1/2  
+     u        +     U       +  u  
+      i-1/2,j |      i,j    |   i+1/2,j 
+              |             |      
+              +------+------+  
+                   n+1/2 
+                  v 
+                    i,j-1/2   
+
+    """
+
+    # this returns u on x-interfaces and v on y-interfaces.  These
+    # constitute the MAC grid
+    u_MAC, v_MAC = incomp_interface_f.mac_vels(myg.qx, myg.qy, myg.ng, 
+                                               myg.dx, myg.dy, dt,
+                                               u, v,
+                                               gradp_x, gradp_y,
+                                               utrans, vtrans)
+
+
+    #-------------------------------------------------------------------------
+    # do a MAC projection ot make the advective velocities divergence
+    # free
+    # -------------------------------------------------------------------------
+
+    # we will solve L phi = D U^MAC, where phi is cell centered, and
+    # U^MAC is the MAC-type staggered grid of the advective
+    # velocities.
+
+    # create the multigrid object
+
+
+    # create the divU source term
+
+    
+    # solve the Poisson problem
+
+
+    # update the normal velocities with the pressure gradient -- these
+    # constitute our advective velocities
+
+
+
+    #-------------------------------------------------------------------------
+    # recompute the interface states, using the advective velocity
+    # from above
+    # -------------------------------------------------------------------------
+    u_x, v_x, u_y, v_y = incomp_interface_f.states(myg.qx, myg.qy, myg.ng, 
+                                                   myg.dx, myg.dy, dt,
+                                                   u, v,
+                                                   gradp_x, gradp_y,
+                                                   utrans, vtrans,
+                                                   uadv, vadv)
+
+
+    #-------------------------------------------------------------------------
+    # compute (U.grad)U
+    #-------------------------------------------------------------------------
+
+
+
+    #-------------------------------------------------------------------------
+    # update U to get the provisional velocity field
+    #-------------------------------------------------------------------------
+
+
+
+    #-------------------------------------------------------------------------
+    # project the final velocity
+    #-------------------------------------------------------------------------
+
+
+
+
+
+    [flux_x, flux_y] =  unsplitFluxes(myData.grid, dt, dens)
+
+    """
+    do the differencing for the fluxes now.  Here, we use slices so we
+    avoid slow loops in python.  This is equivalent to:
+
+    myPatch.data[i,j] = myPatch.data[i,j] + \
+                            dtdx*(flux_x[i,j] - flux_x[i+1,j]) + \
+                            dtdy*(flux_y[i,j] - flux_y[i,j+1])
+    """
+
+    qx = myData.grid.qx
+    qy = myData.grid.qy
+    dens[0:qx-1,0:qy-1] = dens[0:qx-1,0:qy-1] + \
+        dtdx*(flux_x[0:qx-1,0:qy-1] - flux_x[1:qx,0:qy-1]) + \
+        dtdy*(flux_y[0:qx-1,0:qy-1] - flux_y[0:qx-1,1:qy])
+            
+
+    
---
+++ incompressible/incomp_interface_f.f90
@@ -0,0 +1,103 @@
+subroutine trans_vels(qx, qy, ng, dx, dy, dt, &
+                      u, v, &
+                      ldelta_u, ldelta_v, &
+                      utrans, vtrans)
+
+  implicit none
+
+  integer, intent(in) :: qx, qy, ng
+  double precision, intent(in) :: dx, dy, dt
+
+  ! 0-based indexing to match python
+  double precision, intent(inout) :: u(0:qx-1, 0:qy-1)
+  double precision, intent(inout) :: v(0:qx-1, 0:qy-1)
+
+  double precision, intent(inout) :: ldelta_u(0:qx-1, 0:qy-1)
+  double precision, intent(inout) :: ldelta_v(0:qx-1, 0:qy-1)
+
+  double precision, intent(  out) :: utrans(0:qx-1, 0:qy-1, 0:nvar-1)
+  double precision, intent(  out) :: vtrans(0:qx-1, 0:qy-1, 0:nvar-1)
+
+!f2py depend(qx, qy) :: u, v, ldelta_u, ldelta_v
+!f2py depend(qx, qy) :: utrans, vtrans
+!f2py intent(in) :: u, v, ldelta_u, ldelta_v
+!f2py intent(out) :: utrans, vtrans
+ 
+  ! construct the transverse states of u and v --- these will be
+  ! used in constructing the transverse flux difference for the 
+  ! full interface states
+
+  ! specifically, we need u on the x-interfaces (for the transverse
+  ! term in the v normal state) and v on the y-interfaces (for the
+  ! transverse term in the u normal state)
+
+  integer :: ilo, ihi, jlo, jhi
+  integer :: nx, ny
+  integer :: i, j
+
+  double precision :: q_l(0:qx-1, 0:qy-1), q_r(0:qx-1, 0:qy-1)
+
+  
+
+  nx = qx - 2*ng; ny = qy - 2*ng
+  ilo = ng; ihi = ng+nx-1; jlo = ng; jhi = ng+ny-1
+
+
+  ! x-interface states of u
+  dtdx = dt/dx
+
+  do j = jlo-2, jhi+2
+     do i = ilo-2, ihi+2
+        
+        q_l(i+1,j) = u(i,j) + 0.5d0*(1.0d0 - dtdx*u(i,j))*ldelta_u(i,j)
+        q_r(i  ,j) = u(i,j) - 0.5d0*(1.0d0 + dtdx*u(i,j))*ldelta_u(i,j)
+
+     enddo
+  enddo
+
+  ! Riemann problem -- this is based on Burger's equation.  See 
+  ! Bell, Colella, and Howell (1991), Eq. 3.3, or Almgren, Bell,
+  ! and Szymczak (1996) (top of page 362)
+  do j = jlo-1, jhi+1
+     do i = ilo-1, ihi+1
+        if (q_l(i,j) > 0.0d0 .and. (q_l(i,j) + q_r(i,j)) > 0.0d0) then
+           utrans(i,j) = q_l(i,j)
+        else if (q_l(i,j) <= 0.0d0 .and. q_r(i,j) >= 0.0d0) then
+           utrans(i,j) = 0.0d0
+        else
+           utrans(i,j) = q_r(i,j)
+        endif
+     enddo
+  enddo
+
+
+  ! y-interface states of v
+  dtdy = dt/dy
+
+  do j = jlo-2, jhi+2
+     do i = ilo-2, ihi+2
+        
+        q_l(i,j+1) = v(i,j) + 0.5d0*(1.0d0 - dtdy*v(i,j))*ldelta_v(i,j)
+        q_r(i,j  ) = v(i,j) - 0.5d0*(1.0d0 + dtdy*v(i,j))*ldelta_v(i,j)
+
+     enddo
+  enddo
+
+  ! Riemann problem 
+  do j = jlo-1, jhi+1
+     do i = ilo-1, ihi+1
+        if (q_l(i,j) > 0.0d0 .and. (q_l(i,j) + q_r(i,j)) > 0.0d0) then
+           vtrans(i,j) = q_l(i,j)
+        else if (q_l(i,j) <= 0.0d0 .and. q_r(i,j) >= 0.0d0) then
+           vtrans(i,j) = 0.0d0
+        else
+           vtrans(i,j) = q_r(i,j)
+        endif
+     enddo
+  enddo
+
+end subroutine trans_vels
+
+
+
+
