b55a31821b58d2546a4d903d7c9f7e5c750396d0
Michael Zingale <zingale@localhost.localdomain>
2014-06-08 18:26:06 -0400

    add usage (-h) and the ability to specify the output file name

--- analysis/plotvar.py
+++ analysis/plotvar.py
@@ -3,13 +3,15 @@
 import numpy
 import pylab
 import sys
+import getopt
+
 import mesh.patch as patch
 
 # plot a single variable from an output file 
 #
 # Usage: ./plotvar.py filename variable
 
-def makeplot(myd, variable):
+def makeplot(myd, variable, outfile):
 
     pylab.figure(num=1, figsize=(6.5,5.25), dpi=100, facecolor='w')
 
@@ -25,20 +27,48 @@ def makeplot(myd, variable):
     pylab.xlabel("x")
     pylab.ylabel("y")
 
-    pylab.savefig("plot.png", bbox_inches="tight")
+    pylab.savefig(outfile, bbox_inches="tight")
     pylab.show()
+
+
+def usage():
+    usage="""
+usage: plotvar.py [-h] [-o image.png] filename variable
+
+positional arguments:
+  filename      required inputs: filename to read from
+  variable      required inputs: variable to plot from filename
+
+optional arguments:
+  -h, --help    show this help message and exit
+  -o image.png  output image name. The extension .png will generate a PNG
+                file, .eps will generate an EPS file (default: plot.png).
+"""
+    print usage
+    sys.exit()
 
 
 if __name__== "__main__":
 
-    print sys.argv
+    try: opts, next = getopt.getopt(sys.argv[1:], "o:h")
+    except getopt.GetoptError:
+        sys.exit("invalid calling sequence")
 
-    file = sys.argv[1]
-    variable = sys.argv[2]
+    outfile = "plot.png"
+
+    for o, a in opts:
+        if o == "-h": usage()
+        if o == "-o": outfile = a
+                                    
+    try: file = next[0]
+    except: usage()
+        
+    try: variable = next[1]
+    except: usage()
 
     myg, myd = patch.read(file)
 
-    makeplot(myd, variable)
+    makeplot(myd, variable, outfile)
 
 
 
