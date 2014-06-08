12323cb1714c4d00d317a5f6d88d515279269195
Michael Zingale <zingale@localhost.localdomain>
2014-06-08 17:28:38 -0400

    FLOW -> __init__'s docstring

--- advection/__init__.py
+++ advection/__init__.py
@@ -1,3 +1,27 @@
+"""
+The pyro advection solver.  This implements a second-order, unsplit method for
+linear advection based on the Colella 1990 paper.
+
+The general flow of the solver when invoked through pyro.py is:
+
+  create grid
+
+  initial conditions
+
+  main loop
+
+     fill ghost cells
+
+     compute dt
+
+     compute fluxes
+
+     conservative update
+
+     output
+
+"""
+
 __all__ = ['initialize','evolve','preevolve']
 from initialize import *
 from preevolve import *
