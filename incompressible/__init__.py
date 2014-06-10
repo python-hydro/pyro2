3647db4c6969f8557a68a890ed82fa52e98392c9
Michael Zingale <zingale@inf.astro.sunysb.edu>
2014-06-10 11:59:47 -0400

    refactor into a simulation class

--- incompressible/__init__.py
+++ incompressible/__init__.py
@@ -19,9 +19,6 @@ approximate projection method.  The general flow is:
 The projections are done using multigrid
 """
 
-from initialize import *
-from preevolve import *
-from evolve import *
-from timestep import *
-from dovis import *
+from simulation import *
+
 
