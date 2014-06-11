24260581a83a442c7fb31516be7727431216257c
Michael Zingale <zingale@inf.astro.sunysb.edu>
2014-06-11 16:19:20 -0400

    updated convergence #s

--- incompressible/tests/convergence_plot.py
+++ incompressible/tests/convergence_plot.py
@@ -8,7 +8,6 @@ def plot_convergence():
 
     nx = data[:,0]
     errlim = data[:,1]
-    errnolim = data[:,3]
 
     ax = pylab.subplot(111)
     ax.set_xscale('log')
@@ -16,9 +15,6 @@ def plot_convergence():
 
     pylab.scatter(nx, errlim, marker="x", color="r", label="limiting")
     pylab.plot(nx, errlim[0]*(nx[0]/nx)**2, "--", color="k")
-
-    pylab.scatter(nx, errnolim, marker="x", color="b", label="no limiting")
-    pylab.plot(nx, errnolim[0]*(nx[0]/nx)**2, "--", color="k")
 
     pylab.xlabel("number of zones")
     pylab.ylabel("L2 norm of abs error")
@@ -31,12 +27,8 @@ def plot_convergence():
     pylab.xlim(16,256)
     pylab.ylim(2.e-4,5.e-2)
 
-    leg = pylab.legend()
-    leg.draw_frame(0)
-    ltext = leg.get_texts()
-    pylab.setp(ltext, fontsize='small')
-
     pylab.savefig("incomp_converge.png", bbox_inches="tight")
+    pylab.savefig("incomp_converge.eps", bbox_inches="tight")
 
     
 
