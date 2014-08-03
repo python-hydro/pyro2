0044e3a035894311ab47d6d76f3db6e8fce364a7
Michael Zingale <michael.zingale@stonybrook.edu>
2014-08-02 21:09:30 -0400

    flexibility to override default python

--- mk.sh
+++ mk.sh
@@ -4,7 +4,11 @@
 # interface Fortran with python for some lower-level pyro routines.
 # f2py is used.  
 
-PYTHON=python
+# set the python interpreter to use.  If no PYTHON variable is 
+# set, then default to python.  You can use python3, for example,
+# by doing:
+# PYTHON=python3 ./mk.sh
+: ${PYTHON:=python}
 
 cd mesh
 ${PYTHON} setup.py build_ext --inplace
