a65dd7b3083c37cbd28b6a3617f2d27c773c7b25
Michael Zingale <michael.zingale@stonybrook.edu>
2015-01-06 14:05:22 -0500

    numpy -> np; pylab -> plt

--- multigrid/tests/convergence_plot.py
+++ multigrid/tests/convergence_plot.py
@@ -1,38 +1,36 @@
 import math
-import numpy
-import pylab
+import numpy as np
+import matplotlib.pyplot as plt
 
 def plot_convergence():
 
-    data = numpy.loadtxt("mg_convergence.txt")
+    data = np.loadtxt("mg_convergence.txt")
 
     nx = data[:,0]
     err = data[:,1]
 
-    ax = pylab.subplot(111)
+    ax = plt.subplot(111)
     ax.set_xscale('log')
     ax.set_yscale('log')
 
-    pylab.scatter(nx, err, marker="x", color="r")
-    pylab.plot(nx, err[0]*(nx[0]/nx)**2, "--", color="k")
+    plt.scatter(nx, err, marker="x", color="r")
+    plt.plot(nx, err[0]*(nx[0]/nx)**2, "--", color="k")
 
-    pylab.xlabel("number of zones")
-    pylab.ylabel("L2 norm of abs error")
+    plt.xlabel("number of zones")
+    plt.ylabel("L2 norm of abs error")
 
-    pylab.title(r"convergence for multigrid solver", fontsize=11)
+    plt.title(r"convergence for multigrid solver", fontsize=11)
 
-    f = pylab.gcf()
+    f = plt.gcf()
     f.set_size_inches(5.0,5.0)
 
-    pylab.xlim(8,512)
+    plt.xlim(8,512)
 
-    pylab.tight_layout()
+    plt.tight_layout()
 
-    pylab.savefig("mg_converge.png", bbox_inches="tight")
-    pylab.savefig("mg_converge.eps", bbox_inches="tight")
+    plt.savefig("mg_converge.png", bbox_inches="tight")
+    plt.savefig("mg_converge.eps", bbox_inches="tight")
 
-    
 
 if __name__== "__main__":
     plot_convergence()
-
--- multigrid/variable_coeff_MG.py
+++ multigrid/variable_coeff_MG.py
@@ -12,10 +12,10 @@ A cell-centered discretization is used throughout.
 from __future__ import print_function
 
 import multigrid.MG as MG
-import numpy
-import pylab
+import numpy as np
+import matplotlib.pyplot as plt
 
-numpy.set_printoptions(precision=3, linewidth=128)
+np.set_printoptions(precision=3, linewidth=128)
 
 
 class _EdgeCoeffs:
@@ -30,8 +30,8 @@ class _EdgeCoeffs:
             eta_x = g.scratch_array()
             eta_y = g.scratch_array()
 
-            # the eta's are defined on the interfaces, so 
-            # eta_x[i,j] will be eta_{i-1/2,j} and 
+            # the eta's are defined on the interfaces, so
+            # eta_x[i,j] will be eta_{i-1/2,j} and
             # eta_y[i,j] will be eta_{i,j-1/2}
 
             eta_x[g.ilo:g.ihi+2,g.jlo:g.jhi+1] = \
@@ -63,47 +63,47 @@ class _EdgeCoeffs:
         c_eta_y = cg.scratch_array()
 
         fg = self.grid
-        
+
         c_eta_x[cg.ilo:cg.ihi+2,cg.jlo:cg.jhi+1] = \
             0.5*(self.x[fg.ilo:fg.ihi+2:2,fg.jlo  :fg.jhi+1:2] +
-                 self.x[fg.ilo:fg.ihi+2:2,fg.jlo+1:fg.jhi+1:2]) 
-        
+                 self.x[fg.ilo:fg.ihi+2:2,fg.jlo+1:fg.jhi+1:2])
+
         # redo the normalization
         c_edge_coeffs.x = c_eta_x*fg.dx**2/cg.dx**2
 
         c_eta_y[cg.ilo:cg.ihi+1,cg.jlo:cg.jhi+2] = \
             0.5*(self.y[fg.ilo  :fg.ihi+1:2,fg.jlo:fg.jhi+2:2] +
-                 self.y[fg.ilo+1:fg.ihi+1:2,fg.jlo:fg.jhi+2:2]) 
-        
+                 self.y[fg.ilo+1:fg.ihi+1:2,fg.jlo:fg.jhi+2:2])
+
         c_edge_coeffs.y = c_eta_y*fg.dy**2/cg.dy**2
-        
+
         return c_edge_coeffs
 
-        
+
 class VarCoeffCCMG2d(MG.CellCenterMG2d):
     """
     this is a multigrid solver that supports variable coefficients
-    
+
     we need to accept a coefficient array, coeffs, defined at each
     level.  We can do this at the fine level and restrict it
     down the MG grids once.
-    
+
     we need a new compute_residual() and smooth() function, that
     understands coeffs.
     """
-   
+
     def __init__(self, nx, ny, xmin=0.0, xmax=1.0, ymin=0.0, ymax=1.0,
                  xl_BC_type="dirichlet", xr_BC_type="dirichlet",
                  yl_BC_type="dirichlet", yr_BC_type="dirichlet",
                  nsmooth=10, nsmooth_bottom=50,
-                 verbose=0, 
+                 verbose=0,
                  coeffs=None, coeffs_bc=None,
                  true_function=None, vis=0, vis_title=""):
 
         # we'll keep a list of the coefficients averaged to the interfaces
         # on each level
         self.edge_coeffs = []
-       
+
         # initialize the MG object with the auxillary "coeffs" field
         MG.CellCenterMG2d.__init__(self, nx, ny, ng=1,
                                    xmin=xmin, xmax=xmax, ymin=ymin, ymax=ymax,
@@ -113,7 +113,7 @@ class VarCoeffCCMG2d(MG.CellCenterMG2d):
                                    nsmooth=nsmooth, nsmooth_bottom=nsmooth_bottom,
                                    verbose=verbose,
                                    aux_field="coeffs", aux_bc=coeffs_bc,
-                                   true_function=true_function, vis=vis, 
+                                   true_function=true_function, vis=vis,
                                    vis_title=vis_title)
 
 
@@ -121,7 +121,7 @@ class VarCoeffCCMG2d(MG.CellCenterMG2d):
         # we only need to do this once.
         c = self.grids[self.nlevels-1].get_var("coeffs")
         c[:,:] = coeffs.copy()
-        
+
         self.grids[self.nlevels-1].fill_BC("coeffs")
 
         # put the coefficients on edges
@@ -158,10 +158,10 @@ class VarCoeffCCMG2d(MG.CellCenterMG2d):
 
 
     def smooth(self, level, nsmooth):
-        """ 
+        """
         Use red-black Gauss-Seidel iterations to smooth the solution
         at a given level.  This is used at each stage of the V-cycle
-        (up and down) in the MG solution, but it can also be called 
+        (up and down) in the MG solution, but it can also be called
         directly to solve the elliptic problem (although it will take
         many more iterations).
 
@@ -183,9 +183,9 @@ class VarCoeffCCMG2d(MG.CellCenterMG2d):
         eta_x = self.edge_coeffs[level].x
         eta_y = self.edge_coeffs[level].y
 
-        # print( "min/max c: {}, {}".format(numpy.min(c), numpy.max(c)))
-        # print( "min/max eta_x: {}, {}".format(numpy.min(eta_x), numpy.max(eta_x)))
-        # print( "min/max eta_y: {}, {}".format(numpy.min(eta_y), numpy.max(eta_y)))
+        # print( "min/max c: {}, {}".format(np.min(c), np.max(c)))
+        # print( "min/max eta_x: {}, {}".format(np.min(eta_x), np.max(eta_x)))
+        # print( "min/max eta_y: {}, {}".format(np.min(eta_y), np.max(eta_y)))
 
 
         # do red-black G-S
@@ -206,7 +206,7 @@ class VarCoeffCCMG2d(MG.CellCenterMG2d):
             #      --+-------+-------+--
             #        |  ilo  |       |
             #
-            # groups 1 and 3 are done together, then we need to 
+            # groups 1 and 3 are done together, then we need to
             # fill ghost cells, and then groups 2 and 4
 
             for n, (ix, iy) in enumerate([(0,0), (1,1), (1,0), (0,1)]):
@@ -231,7 +231,7 @@ class VarCoeffCCMG2d(MG.CellCenterMG2d):
                           myg.jlo+iy  :myg.jhi+1:2] *
                     v[myg.ilo+1+ix:myg.ihi+2:2,
                       myg.jlo+iy  :myg.jhi+1:2] +
-                    # eta_{i-1/2,j} phi_{i-1,j}                 
+                    # eta_{i-1/2,j} phi_{i-1,j}
                     eta_x[myg.ilo+ix:myg.ihi+1:2,
                           myg.jlo+iy:myg.jhi+1:2]*
                     v[myg.ilo-1+ix:myg.ihi  :2,
@@ -247,31 +247,31 @@ class VarCoeffCCMG2d(MG.CellCenterMG2d):
                     v[myg.ilo+ix  :myg.ihi+1:2,
                       myg.jlo-1+iy:myg.jhi  :2]) / denom
 
-            
+
                 if n == 1 or n == 3:
                     self.grids[level].fill_BC("v")
 
 
             if self.vis == 1:
-                pylab.clf()
+                plt.clf()
 
-                pylab.subplot(221)
+                plt.subplot(221)
                 self._draw_solution()
 
-                pylab.subplot(222)        
+                plt.subplot(222)
                 self._draw_V()
 
-                pylab.subplot(223)        
+                plt.subplot(223)
                 self._draw_main_solution()
 
-                pylab.subplot(224)        
+                plt.subplot(224)
                 self._draw_main_error()
 
 
-                pylab.suptitle(self.vis_title, fontsize=18)
+                plt.suptitle(self.vis_title, fontsize=18)
 
-                pylab.draw()
-                pylab.savefig("mg_%4.4d.png" % (self.frame))
+                plt.draw()
+                plt.savefig("mg_%4.4d.png" % (self.frame))
                 self.frame += 1
 
 
@@ -289,12 +289,12 @@ class VarCoeffCCMG2d(MG.CellCenterMG2d):
         eta_y = self.edge_coeffs[level].y
 
 
-        # compute the residual 
+        # compute the residual
         # r = f - L_eta phi
-        L_eta_phi = ( 
+        L_eta_phi = (
             # eta_{i+1/2,j} (phi_{i+1,j} - phi_{i,j})
             eta_x[myg.ilo+1:myg.ihi+2,myg.jlo:myg.jhi+1]* \
-            (v[myg.ilo+1:myg.ihi+2,myg.jlo:myg.jhi+1] - 
+            (v[myg.ilo+1:myg.ihi+2,myg.jlo:myg.jhi+1] -
              v[myg.ilo  :myg.ihi+1,myg.jlo:myg.jhi+1]) - \
             # eta_{i-1/2,j} (phi_{i,j} - phi_{i-1,j})
             eta_x[myg.ilo  :myg.ihi+1,myg.jlo:myg.jhi+1]* \
@@ -311,6 +311,3 @@ class VarCoeffCCMG2d(MG.CellCenterMG2d):
 
         r[myg.ilo:myg.ihi+1,myg.jlo:myg.jhi+1] = \
             f[myg.ilo:myg.ihi+1,myg.jlo:myg.jhi+1] - L_eta_phi
-
-
-
