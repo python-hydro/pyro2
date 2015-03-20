ef0a097b00515d91b70b241d76cc289cc5eb6e11
Michael Zingale <michael.zingale@stonybrook.edu>
2015-03-19 22:30:37 -0400

    better output checking

--- test.py
+++ test.py
@@ -2,33 +2,56 @@
 
 import pyro
 
+results = {}
+
+
 opts = "driver.verbose=0 vis.dovis=0".split()
 
 # advection
 add_err = pyro.doit("advection", "smooth", "inputs.smooth",
   		    other_commands=opts, comp_bench=True)
- 
+
+results["advection"] = add_err
+
+
 # compressible
 comp_err = pyro.doit("compressible", "quad", "inputs.quad",
   		     other_commands=opts, comp_bench=True)
+
+results["compressible"] = comp_err
+
+
 
 # diffusion
 diff_err = pyro.doit("diffusion", "gaussian", "inputs.gaussian",
   		     other_commands=opts, comp_bench=True)
 
+results["diffusion"] = diff_err
+
+
 # incompressible
 incomp_err = pyro.doit("incompressible", "shear", "inputs.shear",
   		     other_commands=opts, comp_bench=True)
+
+results["incompressible"] = incomp_err
+
 
 # LM_atm
 lm_err = pyro.doit("lm_atm", "bubble", "inputs.bubble",
   		   other_commands=opts, comp_bench=True)
 
+results["lm_atm"] = lm_err
 
-if not add_err == 0: print "advection problem failed"
-if not comp_err == 0: print "compressible problem failed"
-if not diff_err == 0: print "diffusion problem failed"
-if not incomp_err == 0: print "incompressible problem failed"
-if not lm_err == 0: print "lm_atm problem failed"
+
+for s, r in results.iteritems():
+    if not r == 0:
+        print "{:32} failed".format(s)
+    else:
+        print "{:32} passed".format(s)
+
+
+
+
+
 
 
