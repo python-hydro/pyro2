63e85b436fe5727055b701316ee7b7d8ec1db2ad
Michael Zingale <michael.zingale@stonybrook.edu>
2015-03-20 15:03:35 -0400

    start of a more general MG solver

---
+++ multigrid/general_MG.py
@@ -0,0 +1,253 @@
+"""
+This multigrid solver is build from multigrid/MG.py 
+and implements a more general solver for an equation of the form:
+
+alpha phi + div { beta grad phi } + gamma . grad phi = f
+
+where alpha, beta, and gamma are defined on the same grid as phi.
+These should all come in as cell-centered quantities.  The solver
+will put beta on edges.  Note that gamma is a vector here, with
+x- and y-components.
+
+A cell-centered discretization for phi is used throughout.
+"""
+
+from __future__ import print_function
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+import multigrid.MG as MG
+import multigrid.variable_coeff_MG as vcMG
+
+np.set_printoptions(precision=3, linewidth=128)
+
+
+class GeneralMG2d(MG.CellCenteredG2d):
+    """
+    this is a multigrid solver that supports variable coefficients
+
+    we need to accept a coefficient array, coeffs, defined at each
+    level.  We can do this at the fine level and restrict it
+    down the MG grids once.
+
+    we need a new compute_residual() and smooth() function, that
+    understands coeffs.
+    """
+
+    def __init__(self, nx, ny, xmin=0.0, xmax=1.0, ymin=0.0, ymax=1.0,
+                 xl_BC_type="dirichlet", xr_BC_type="dirichlet",
+                 yl_BC_type="dirichlet", yr_BC_type="dirichlet",
+                 nsmooth=10, nsmooth_bottom=50,
+                 verbose=0,
+                 coeffs=None, coeffs_bc=None,
+                 true_function=None, vis=0, vis_title=""):
+        """
+        here, coeffs is a dictionary
+        """
+        
+        # we'll keep a list of the beta coefficients averaged to the
+        # interfaces on each level
+        self.beta_edge = []
+
+        # initialize the MG object with the auxillary fields
+        MG.CellCenterMG2d.__init__(self, nx, ny, ng=1,
+                                   xmin=xmin, xmax=xmax, ymin=ymin, ymax=ymax,
+                                   xl_BC_type=xl_BC_type, xr_BC_type=xr_BC_type,
+                                   yl_BC_type=yl_BC_type, yr_BC_type=yr_BC_type,
+                                   alpha=0.0, beta=0.0,
+                                   nsmooth=nsmooth, nsmooth_bottom=nsmooth_bottom,
+                                   verbose=verbose,
+                                   aux_field=["alpha", "beta", "gamma_x", "gamma_y"]
+                                   aux_bc=[coeffs_bc["alpha"], coeffs_bc["beta"],
+                                           coeffs_bc["gamma_x"], coeffs_bc["gamma_y"]],
+                                   true_function=true_function, vis=vis,
+                                   vis_title=vis_title)
+
+
+        # the coefficents come in a dictionary.  Set the coefficients
+        # and restrict them down the hierarchy we only need to do this
+        # once.  We need to hold the original coeffs in our grid so we
+        # can do a ghost cell fill.
+        for c in ["alpha", "beta", "gamma_x", "gamma_y"]:
+            v = self.grids[self.nlevels-1].get_var(c)
+            v[:,:] = coeffs[c].copy()
+
+            self.grids[self.nlevels-1].fill_BC(c)
+
+            n = self.nlevels-2
+            while n >= 0:
+                f_patch = self.grids[n+1]
+                c_patch = self.grids[n]
+
+                coeffs_c = c_patch.get_var(c)
+                coeffs_c[:,:] = f_patch.restrict(c)
+
+                self.grids[n].fill_BC(c)
+
+
+        # put the beta coefficients on edges
+        beta = self.grids[self.nlevels-1].get_var("beta")
+        self.edge_coeffs.insert(0, ec.EdgeCoeffs(self.grids[self.nlevels-1].grid, beta))
+
+        n = self.nlevels-2
+        while n >= 0:
+            self.beta_edge.insert(0, self.edge_coeffs[0].restrict())
+            n -= 1
+
+
+
+    def smooth(self, level, nsmooth):
+        """
+        Use red-black Gauss-Seidel iterations to smooth the solution
+        at a given level.  This is used at each stage of the V-cycle
+        (up and down) in the MG solution, but it can also be called
+        directly to solve the elliptic problem (although it will take
+        many more iterations).
+
+        Parameters
+        ----------
+        level : int
+            The level in the MG hierarchy to smooth the solution
+        nsmooth : int
+            The number of r-b Gauss-Seidel smoothing iterations to perform
+
+        """
+        v = self.grids[level].get_var("v")
+        f = self.grids[level].get_var("f")
+
+        myg = self.grids[level].grid
+
+        self.grids[level].fill_BC("v")
+
+        eta_x = self.edge_coeffs[level].x
+        eta_y = self.edge_coeffs[level].y
+
+        # print( "min/max c: {}, {}".format(np.min(c), np.max(c)))
+        # print( "min/max eta_x: {}, {}".format(np.min(eta_x), np.max(eta_x)))
+        # print( "min/max eta_y: {}, {}".format(np.min(eta_y), np.max(eta_y)))
+
+
+        # do red-black G-S
+        for i in range(nsmooth):
+
+            # do the red black updating in four decoupled groups
+            #
+            #
+            #        |       |       |
+            #      --+-------+-------+--
+            #        |       |       |
+            #        |   4   |   3   |
+            #        |       |       |
+            #      --+-------+-------+--
+            #        |       |       |
+            #   jlo  |   1   |   2   |
+            #        |       |       |
+            #      --+-------+-------+--
+            #        |  ilo  |       |
+            #
+            # groups 1 and 3 are done together, then we need to
+            # fill ghost cells, and then groups 2 and 4
+
+            for n, (ix, iy) in enumerate([(0,0), (1,1), (1,0), (0,1)]):
+
+                denom = (
+                    eta_x[myg.ilo+1+ix:myg.ihi+2:2,
+                          myg.jlo+iy  :myg.jhi+1:2] +
+                    #
+                    eta_x[myg.ilo+ix  :myg.ihi+1:2,
+                          myg.jlo+iy  :myg.jhi+1:2] +
+                    #
+                    eta_y[myg.ilo+ix  :myg.ihi+1:2,
+                          myg.jlo+1+iy:myg.jhi+2:2] +
+                    #
+                    eta_y[myg.ilo+ix  :myg.ihi+1:2,
+                          myg.jlo+iy  :myg.jhi+1:2])
+
+                v[myg.ilo+ix:myg.ihi+1:2,myg.jlo+iy:myg.jhi+1:2] = (
+                    -f[myg.ilo+ix:myg.ihi+1:2,myg.jlo+iy:myg.jhi+1:2] +
+                    # eta_{i+1/2,j} phi_{i+1,j}
+                    eta_x[myg.ilo+1+ix:myg.ihi+2:2,
+                          myg.jlo+iy  :myg.jhi+1:2] *
+                    v[myg.ilo+1+ix:myg.ihi+2:2,
+                      myg.jlo+iy  :myg.jhi+1:2] +
+                    # eta_{i-1/2,j} phi_{i-1,j}
+                    eta_x[myg.ilo+ix:myg.ihi+1:2,
+                          myg.jlo+iy:myg.jhi+1:2]*
+                    v[myg.ilo-1+ix:myg.ihi  :2,
+                      myg.jlo+iy  :myg.jhi+1:2] +
+                    # eta_{i,j+1/2} phi_{i,j+1}
+                    eta_y[myg.ilo+ix:myg.ihi+1:2,
+                          myg.jlo+1+iy:myg.jhi+2:2]*
+                    v[myg.ilo+ix  :myg.ihi+1:2,
+                      myg.jlo+1+iy:myg.jhi+2:2] +
+                    # eta_{i,j-1/2} phi_{i,j-1}
+                    eta_y[myg.ilo+ix:myg.ihi+1:2,
+                          myg.jlo+iy:myg.jhi+1:2]*
+                    v[myg.ilo+ix  :myg.ihi+1:2,
+                      myg.jlo-1+iy:myg.jhi  :2]) / denom
+
+
+                if n == 1 or n == 3:
+                    self.grids[level].fill_BC("v")
+
+
+            if self.vis == 1:
+                plt.clf()
+
+                plt.subplot(221)
+                self._draw_solution()
+
+                plt.subplot(222)
+                self._draw_V()
+
+                plt.subplot(223)
+                self._draw_main_solution()
+
+                plt.subplot(224)
+                self._draw_main_error()
+
+
+                plt.suptitle(self.vis_title, fontsize=18)
+
+                plt.draw()
+                plt.savefig("mg_%4.4d.png" % (self.frame))
+                self.frame += 1
+
+
+    def _compute_residual(self, level):
+        """ compute the residual and store it in the r variable"""
+
+
+        v = self.grids[level].get_var("v")
+        f = self.grids[level].get_var("f")
+        r = self.grids[level].get_var("r")
+
+        myg = self.grids[level].grid
+
+        eta_x = self.edge_coeffs[level].x
+        eta_y = self.edge_coeffs[level].y
+
+
+        # compute the residual
+        # r = f - L_eta phi
+        L_eta_phi = (
+            # eta_{i+1/2,j} (phi_{i+1,j} - phi_{i,j})
+            eta_x[myg.ilo+1:myg.ihi+2,myg.jlo:myg.jhi+1]* \
+            (v[myg.ilo+1:myg.ihi+2,myg.jlo:myg.jhi+1] -
+             v[myg.ilo  :myg.ihi+1,myg.jlo:myg.jhi+1]) - \
+            # eta_{i-1/2,j} (phi_{i,j} - phi_{i-1,j})
+            eta_x[myg.ilo  :myg.ihi+1,myg.jlo:myg.jhi+1]* \
+            (v[myg.ilo  :myg.ihi+1,myg.jlo:myg.jhi+1] -
+             v[myg.ilo-1:myg.ihi  ,myg.jlo:myg.jhi+1]) + \
+            # eta_{i,j+1/2} (phi_{i,j+1} - phi_{i,j})
+            eta_y[myg.ilo:myg.ihi+1,myg.jlo+1:myg.jhi+2]* \
+            (v[myg.ilo:myg.ihi+1,myg.jlo+1:myg.jhi+2] -  # y-diff
+             v[myg.ilo:myg.ihi+1,myg.jlo  :myg.jhi+1]) - \
+            # eta_{i,j-1/2} (phi_{i,j} - phi_{i,j-1})
+            eta_y[myg.ilo:myg.ihi+1,myg.jlo  :myg.jhi+1]* \
+            (v[myg.ilo:myg.ihi+1,myg.jlo  :myg.jhi+1] -
+             v[myg.ilo:myg.ihi+1,myg.jlo-1:myg.jhi  ]) )
+
+        r[myg.ilo:myg.ihi+1,myg.jlo:myg.jhi+1] = \
+            f[myg.ilo:myg.ihi+1,myg.jlo:myg.jhi+1] - L_eta_phi
