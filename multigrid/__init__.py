718c4338a8c3799dafb0909a580ef3a863df9e57
Michael Zingale <michael.zingale@stonybrook.edu>
2015-03-20 14:16:34 -0400

    update docstring

--- multigrid/__init__.py
+++ multigrid/__init__.py
@@ -1,11 +1,22 @@
-"""
-This is the pyro multigrid solver.  It uses a second-order discretization
-of a constant-coefficient Helmholtz equation:
+"""This is the pyro multigrid solver.  THere are several versions.
+
+MG implements a second-order discretization of a constant-coefficient
+Helmholtz equation:
 
  (alpha - beta L) phi = f
 
-and pure V-cycles to solve elliptic problems
+
+variable_coeff_MG implements a variable-coefficient Poisson equation:
+
+  div { eta grad phi } = f
+
+general_MG implements a more general elliptic equation:
+
+  alpha phi + div { beta grad phi } + gamma . grad phi = f
+
+
+All use pure V-cycles to solve elliptic problems
+
 """
 
 __all__ = ['MG', 'variable_coeff_MG']
-
