3c5c3a5ae68682faa29ba2dac75901ec14fa7528
Michael Zingale <michael.zingale@stonybrook.edu>
2015-03-23 21:08:10 -0400

    update with the ability to report to a file

--- test.py
+++ test.py
@@ -2,6 +2,10 @@
 
 from __future__ import print_function
 
+import argparse
+import datetime
+import sys
+
 import pyro
 import multigrid.test_mg as test_mg
 import multigrid.test_mg_vc_dirichlet as test_mg_vc_dirichlet
@@ -15,43 +19,67 @@ class test:
         self.options = options
 
 
-opts = "driver.verbose=0 vis.dovis=0 io.do_io=0".split()
+def do_tests(out_file):
+    
+    opts = "driver.verbose=0 vis.dovis=0 io.do_io=0".split()
 
-tests = []
-tests.append(test("advection", "smooth", "inputs.smooth", opts))
-tests.append(test("compressible", "quad", "inputs.quad", opts))
-tests.append(test("diffusion", "gaussian", "inputs.gaussian", opts))
-tests.append(test("incompressible", "shear", "inputs.shear", opts))
-tests.append(test("lm_atm", "bubble", "inputs.bubble", opts))
-
-
-results = {}
-
-for t in tests:
-    err = pyro.doit(t.solver, t.problem, t.inputs,
-                    other_commands=t.options, comp_bench=True)
-    results[t.solver] = err
+    tests = []
+    tests.append(test("advection", "smooth", "inputs.smooth", opts))
+    tests.append(test("compressible", "quad", "inputs.quad", opts))
+    tests.append(test("diffusion", "gaussian", "inputs.gaussian", opts))
+    tests.append(test("incompressible", "shear", "inputs.shear", opts))
+    tests.append(test("lm_atm", "bubble", "inputs.bubble", opts))
 
 
+    results = {}
 
-# standalone tests
-err = test_mg.test_poisson_dirichlet(256, comp_bench=True)
-results["mg_poisson_dirichlet"] = err
-
-err = test_mg_vc_dirichlet.test_vc_poisson_dirichlet(512, comp_bench=True)
-results["mg_vc_poisson_dirichlet"] = err
-
-err = test_mg_vc_periodic.test_vc_poisson_periodic(512, comp_bench=True)
-results["mg_vc_poisson_periodic"] = err
+    for t in tests:
+        err = pyro.doit(t.solver, t.problem, t.inputs,
+                        other_commands=t.options, comp_bench=True)
+        results[t.solver] = err
 
 
-failed = 0
-for s, r in results.iteritems():
-    if not r == 0:
-        print("{:32} failed".format(s))
-        failed += 1
-    else:
-        print("{:32} passed".format(s))
+    # standalone tests
+    err = test_mg.test_poisson_dirichlet(256, comp_bench=True)
+    results["mg_poisson_dirichlet"] = err
+
+    err = test_mg_vc_dirichlet.test_vc_poisson_dirichlet(512, comp_bench=True)
+    results["mg_vc_poisson_dirichlet"] = err
+
+    err = test_mg_vc_periodic.test_vc_poisson_periodic(512, comp_bench=True)
+    results["mg_vc_poisson_periodic"] = err
 
 
-print("\n{} tests failed".format(failed))
+    failed = 0
+
+    f = open(out_file, "w") if out_file else sys.stdout
+
+    f.write("pyro tests run: {}\n\n".format(str(datetime.datetime.now().replace(microsecond=0))))
+    
+    for s, r in sorted(results.items()):
+        if not r == 0:
+            f.write("{:32} failed\n".format(s))
+            failed += 1
+        else:
+            f.write("{:32} passed\n".format(s))
+
+
+    f.write("\n{} test(s) failed\n".format(failed))
+
+    if out_file: f.close()
+    
+
+if __name__ == "__main__":
+
+    p = argparse.ArgumentParser()
+
+    p.add_argument("-o",
+                   help="name of file to output the report to (otherwise output to the screen",
+                   type=str, nargs=1)
+
+    args = p.parse_args()
+
+    do_tests(args.o[0])
+
+
+        
