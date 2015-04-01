eda9b0ad9f54002abe77006cc3717d7b483bf281
Michael Zingale <michael.zingale@stonybrook.edu>
2015-04-01 08:56:43 -0400

    style

--- compressible/unsplitFluxes.py
+++ compressible/unsplitFluxes.py
@@ -3,82 +3,82 @@ Implementation of the Colella 2nd order unsplit Godunov scheme.  This
 is a 2-dimensional implementation only.  We assume that the grid is
 uniform, but it is relatively straightforward to relax this
 assumption.
-                                                                               
+
 There are several different options for this solver (they are all
 discussed in the Colella paper).
 
   limiter          = 0 to use no limiting
-                   = 1 to use the 2nd order MC limiter 
+                   = 1 to use the 2nd order MC limiter
                    = 2 to use the 4th order MC limiter
 
   riemann          = HLLC to use the HLLC solver
                    = CGF to use the Colella, Glaz, and Ferguson solver
 
-  use_flattening   = 1 to use the multidimensional flattening 
+  use_flattening   = 1 to use the multidimensional flattening
                      algorithm at shocks
 
   delta, z0, z1      these are the flattening parameters.  The default
                      are the values listed in Colella 1990.
 
-   j+3/2--+---------+---------+---------+ 
+   j+3/2--+---------+---------+---------+
           |         |         |         |
      j+1 _|         |         |         |
           |         |         |         |
           |         |         |         |
-   j+1/2--+---------XXXXXXXXXXX---------+ 
+   j+1/2--+---------XXXXXXXXXXX---------+
           |         X         X         |
        j _|         X         X         |
           |         X         X         |
-          |         X         X         | 
-   j-1/2--+---------XXXXXXXXXXX---------+ 
+          |         X         X         |
+   j-1/2--+---------XXXXXXXXXXX---------+
           |         |         |         |
      j-1 _|         |         |         |
           |         |         |         |
           |         |         |         |
-   j-3/2--+---------+---------+---------+ 
-          |    |    |    |    |    |    | 
-              i-1        i        i+1  
-        i-3/2     i-1/2     i+1/2     i+3/2  
+   j-3/2--+---------+---------+---------+
+          |    |    |    |    |    |    |
+              i-1        i        i+1
+        i-3/2     i-1/2     i+1/2     i+3/2
 
-We wish to solve  
+We wish to solve
 
   U_t + F^x_x + F^y_y = H
-       
+
 we want U_{i+1/2}^{n+1/2} -- the interface values that are input to
 the Riemann problem through the faces for each zone.
 
-Taylor expanding yields                                                        
-                  
+Taylor expanding yields
+
    n+1/2                     dU           dU
-  U          = U   + 0.5 dx  --  + 0.5 dt -- 
-   i+1/2,j,L    i,j          dx           dt 
-                                                                               
- 
-                             dU             dF^x   dF^y 
-             = U   + 0.5 dx  --  - 0.5 dt ( ---- + ---- - H )  
-                i,j          dx              dx     dy  
+  U          = U   + 0.5 dx  --  + 0.5 dt --
+   i+1/2,j,L    i,j          dx           dt
+
+
+                             dU             dF^x   dF^y
+             = U   + 0.5 dx  --  - 0.5 dt ( ---- + ---- - H )
+                i,j          dx              dx     dy
 
 
                               dU      dF^x            dF^y
-             = U   + 0.5 ( dx -- - dt ---- ) - 0.5 dt ---- + 0.5 dt H 
-                i,j           dx       dx              dy    
-                                                                          
-
-                                  dt       dU           dF^y    
-             = U   + 0.5 dx ( 1 - -- A^x ) --  - 0.5 dt ---- + 0.5 dt H 
-                i,j               dx       dx            dy   
+             = U   + 0.5 ( dx -- - dt ---- ) - 0.5 dt ---- + 0.5 dt H
+                i,j           dx       dx              dy
 
 
-                                dt       _            dF^y   
-             = U   + 0.5  ( 1 - -- A^x ) DU  - 0.5 dt ---- + 0.5 dt H   
-                i,j             dx                     dy      
+                                  dt       dU           dF^y
+             = U   + 0.5 dx ( 1 - -- A^x ) --  - 0.5 dt ---- + 0.5 dt H
+                i,j               dx       dx            dy
 
-                     +----------+-----------+  +----+----+   +---+---+  
-                                |                   |            |   
 
-                    this is the monotonized   this is the   source term  
-                    central difference term   transverse    
-                                              flux term 
+                                dt       _            dF^y
+             = U   + 0.5  ( 1 - -- A^x ) DU  - 0.5 dt ---- + 0.5 dt H
+                i,j             dx                     dy
+
+                     +----------+-----------+  +----+----+   +---+---+
+                                |                   |            |
+
+                    this is the monotonized   this is the   source term
+                    central difference term   transverse
+                                              flux term
 
 There are two components, the central difference in the normal to the
 interface, and the transverse flux difference.  This is done for the
@@ -86,38 +86,38 @@ left and right sides of all 4 interfaces in a zone, which are then
 used as input to the Riemann problem, yielding the 1/2 time interface
 values,
 
-     n+1/2 
-    U  
-     i+1/2,j   
+     n+1/2
+    U
+     i+1/2,j
 
 Then, the zone average values are updated in the usual finite-volume
-way:     
+way:
 
-    n+1    n     dt    x  n+1/2       x  n+1/2 
-   U    = U    + -- { F (U       ) - F (U       ) } 
-    i,j    i,j   dx       i-1/2,j        i+1/2,j   
+    n+1    n     dt    x  n+1/2       x  n+1/2
+   U    = U    + -- { F (U       ) - F (U       ) }
+    i,j    i,j   dx       i-1/2,j        i+1/2,j
 
 
-                 dt    y  n+1/2       y  n+1/2 
-               + -- { F (U       ) - F (U       ) }  
-                 dy       i,j-1/2        i,j+1/2  
+                 dt    y  n+1/2       y  n+1/2
+               + -- { F (U       ) - F (U       ) }
+                 dy       i,j-1/2        i,j+1/2
 
-Updating U_{i,j}: 
+Updating U_{i,j}:
 
   -- We want to find the state to the left and right (or top and
      bottom) of each interface, ex. U_{i+1/2,j,[lr]}^{n+1/2}, and use
      them to solve a Riemann problem across each of the four
      interfaces.
-                                                                               
+
   -- U_{i+1/2,j,[lr]}^{n+1/2} is comprised of two parts, the
      computation of the monotonized central differences in the normal
      direction (eqs. 2.8, 2.10) and the computation of the transverse
      derivatives, which requires the solution of a Riemann problem in
      the transverse direction (eqs. 2.9, 2.14).
-                                                                               
+
        -- the monotonized central difference part is computed using
           the primitive variables.
-                                                                               
+
        -- We compute the central difference part in both directions
           before doing the transverse flux differencing, since for the
           high-order transverse flux implementation, we use these as
@@ -134,11 +134,11 @@ def unsplitFluxes(my_data, rp, vars, tc, dt):
     unsplitFluxes returns the fluxes through the x and y interfaces by
     doing an unsplit reconstruction of the interface values and then
     solving the Riemann problem through all the interfaces at once
-                                                                               
-    currently we assume a gamma-law EOS 
 
-    The runtime parameter grav is assumed to be the gravitational 
-    acceleration in the y-direction            
+    currently we assume a gamma-law EOS
+
+    The runtime parameter grav is assumed to be the gravitational
+    acceleration in the y-direction
 
     Parameters
     ----------
@@ -146,25 +146,25 @@ def unsplitFluxes(my_data, rp, vars, tc, dt):
         The data object containing the grid and advective scalar that
         we are advecting.
     rp : RuntimeParameters object
-        The runtime parameters for the simulation                              
+        The runtime parameters for the simulation
     vars : Variables object
         The Variables object that tells us which indices refer to which
         variables
     tc : TimerCollection object
         The timers we are using to profile
     dt : float
-        The timestep we are advancing through. 
+        The timestep we are advancing through.
 
     Returns
     -------
-    out : ndarray, ndarray 
+    out : ndarray, ndarray
         The fluxes on the x- and y-interfaces
 
     """
 
     tm_flux = tc.timer("unsplitFluxes")
     tm_flux.begin()
-    
+
     myg = my_data.grid
 
     gamma = rp.get_param("eos.gamma")
@@ -192,7 +192,7 @@ def unsplitFluxes(my_data, rp, vars, tc, dt):
 
     smallp = 1.e-10
     p = p.clip(smallp)   # apply a floor to the pressure
-    
+
 
     #=========================================================================
     # compute the flattening coefficients
@@ -226,7 +226,7 @@ def unsplitFluxes(my_data, rp, vars, tc, dt):
         limitFunc = reconstruction_f.limit2
     else:
         limitFunc = reconstruction_f.limit4
-    
+
     ldelta_rx = xi*limitFunc(1, r, myg.qx, myg.qy, myg.ng)
     ldelta_ux = xi*limitFunc(1, u, myg.qx, myg.qy, myg.ng)
     ldelta_vx = xi*limitFunc(1, v, myg.qx, myg.qy, myg.ng)
@@ -246,7 +246,7 @@ def unsplitFluxes(my_data, rp, vars, tc, dt):
     # x-direction
     #=========================================================================
 
-    
+
     # left and right primitive variable states
     tm_states = tc.timer("interfaceStates")
     tm_states.begin()
@@ -258,10 +258,10 @@ def unsplitFluxes(my_data, rp, vars, tc, dt):
                                   vars.nvar,
                                   gamma,
                                   r, u, v, p,
-                                  ldelta_rx, ldelta_ux, ldelta_vx, ldelta_px) 
-    
+                                  ldelta_rx, ldelta_ux, ldelta_vx, ldelta_px)
+
     tm_states.end()
-                    
+
 
     # transform interface states back into conserved variables
     U_xl = myg.scratch_array(vars.nvar)
@@ -285,7 +285,7 @@ def unsplitFluxes(my_data, rp, vars, tc, dt):
     # y-direction
     #=========================================================================
 
-    
+
     # left and right primitive variable states
     tm_states.begin()
 
@@ -293,7 +293,7 @@ def unsplitFluxes(my_data, rp, vars, tc, dt):
                                   vars.nvar,
                                   gamma,
                                   r, u, v, p,
-                                  ldelta_ry, ldelta_uy, ldelta_vy, ldelta_py)  
+                                  ldelta_ry, ldelta_uy, ldelta_vy, ldelta_py)
 
     tm_states.end()
 
@@ -365,12 +365,12 @@ def unsplitFluxes(my_data, rp, vars, tc, dt):
         msg.fail("ERROR: Riemann solver undefined")
 
 
-    F_x = riemannFunc(1, myg.qx, myg.qy, myg.ng, 
-                      vars.nvar, vars.idens, vars.ixmom, vars.iymom, vars.iener, 
+    F_x = riemannFunc(1, myg.qx, myg.qy, myg.ng,
+                      vars.nvar, vars.idens, vars.ixmom, vars.iymom, vars.iener,
                       gamma, U_xl, U_xr)
 
-    F_y = riemannFunc(2, myg.qx, myg.qy, myg.ng, 
-                      vars.nvar, vars.idens, vars.ixmom, vars.iymom, vars.iener, 
+    F_y = riemannFunc(2, myg.qx, myg.qy, myg.ng,
+                      vars.nvar, vars.idens, vars.ixmom, vars.iymom, vars.iener,
                       gamma, U_yl, U_yr)
 
     tm_riem.end()
@@ -386,40 +386,40 @@ def unsplitFluxes(my_data, rp, vars, tc, dt):
 
     The states that we represent by indices i,j are shown below
     (1,2,3,4):
-            
 
-      j+3/2--+----------+----------+----------+ 
-             |          |          |          | 
-             |          |          |          | 
-        j+1 -+          |          |          | 
-             |          |          |          | 
-             |          |          |          |    1: U_xl[i,j,:] = U  
+
+      j+3/2--+----------+----------+----------+
+             |          |          |          |
+             |          |          |          |
+        j+1 -+          |          |          |
+             |          |          |          |
+             |          |          |          |    1: U_xl[i,j,:] = U
       j+1/2--+----------XXXXXXXXXXXX----------+                      i-1/2,j,L
-             |          X          X          | 
-             |          X          X          |  
-          j -+        1 X 2        X          |    2: U_xr[i,j,:] = U 
+             |          X          X          |
+             |          X          X          |
+          j -+        1 X 2        X          |    2: U_xr[i,j,:] = U
              |          X          X          |                      i-1/2,j,R
-             |          X    4     X          | 
-      j-1/2--+----------XXXXXXXXXXXX----------+  
-             |          |    3     |          |    3: U_yl[i,j,:] = U 
+             |          X    4     X          |
+      j-1/2--+----------XXXXXXXXXXXX----------+
+             |          |    3     |          |    3: U_yl[i,j,:] = U
              |          |          |          |                      i,j-1/2,L
-        j-1 -+          |          |          |   
-             |          |          |          |  
-             |          |          |          |    4: U_yr[i,j,:] = U 
+        j-1 -+          |          |          |
+             |          |          |          |
+             |          |          |          |    4: U_yr[i,j,:] = U
       j-3/2--+----------+----------+----------+                      i,j-1/2,R
-             |    |     |    |     |    |     |  
-                 i-1         i         i+1      
-           i-3/2      i-1/2      i+1/2      i+3/2 
+             |    |     |    |     |    |     |
+                 i-1         i         i+1
+           i-3/2      i-1/2      i+1/2      i+3/2
 
 
-    remember that the fluxes are stored on the left edge, so 
+    remember that the fluxes are stored on the left edge, so
 
-    F_x[i,j,:] = F_x 
-                    i-1/2, j   
+    F_x[i,j,:] = F_x
+                    i-1/2, j
 
-    F_y[i,j,:] = F_y   
-                    i, j-1/2   
-                                       
+    F_y[i,j,:] = F_y
+                    i, j-1/2
+
     """
 
     tm_transverse = tc.timer("transverse flux addition")
@@ -428,18 +428,18 @@ def unsplitFluxes(my_data, rp, vars, tc, dt):
     # U_xl[i,j,:] = U_xl[i,j,:] - 0.5*dt/dy * (F_y[i-1,j+1,:] - F_y[i-1,j,:])
     U_xl[myg.ilo-2:myg.ihi+2,myg.jlo-2:myg.jhi+2,:] += \
         - 0.5*dt/myg.dy * (F_y[myg.ilo-3:myg.ihi+1,myg.jlo-1:myg.jhi+3,:] - \
-                           F_y[myg.ilo-3:myg.ihi+1,myg.jlo-2:myg.jhi+2,:])    
+                           F_y[myg.ilo-3:myg.ihi+1,myg.jlo-2:myg.jhi+2,:])
 
     # U_xr[i,j,:] = U_xr[i,j,:] - 0.5*dt/dy * (F_y[i,j+1,:] - F_y[i,j,:])
     U_xr[myg.ilo-2:myg.ihi+2,myg.jlo-2:myg.jhi+2,:] += \
         - 0.5*dt/myg.dy * (F_y[myg.ilo-2:myg.ihi+2,myg.jlo-1:myg.jhi+3,:] - \
-                           F_y[myg.ilo-2:myg.ihi+2,myg.jlo-2:myg.jhi+2,:])    
+                           F_y[myg.ilo-2:myg.ihi+2,myg.jlo-2:myg.jhi+2,:])
 
     # U_yl[i,j,:] = U_yl[i,j,:] - 0.5*dt/dx * (F_x[i+1,j-1,:] - F_x[i,j-1,:])
     U_yl[myg.ilo-2:myg.ihi+2,myg.jlo-2:myg.jhi+2,:] += \
         - 0.5*dt/myg.dx * (F_x[myg.ilo-1:myg.ihi+3,myg.jlo-3:myg.jhi+1,:] - \
                            F_x[myg.ilo-2:myg.ihi+2,myg.jlo-3:myg.jhi+1,:])
-                
+
     # U_yr[i,j,:] = U_yr[i,j,:] - 0.5*dt/dx * (F_x[i+1,j,:] - F_x[i,j,:])
     U_yr[myg.ilo-2:myg.ihi+2,myg.jlo-2:myg.jhi+2,:] += \
         - 0.5*dt/myg.dx * (F_x[myg.ilo-1:myg.ihi+3,myg.jlo-2:myg.jhi+2,:] - \
@@ -451,18 +451,18 @@ def unsplitFluxes(my_data, rp, vars, tc, dt):
     #=========================================================================
     # construct the fluxes normal to the interfaces
     #=========================================================================
-    
+
     # up until now, F_x and F_y stored the transverse fluxes, now we
     # overwrite with the fluxes normal to the interfaces
 
     tm_riem.begin()
-        
-    F_x = riemannFunc(1, myg.qx, myg.qy, myg.ng, 
-                      vars.nvar, vars.idens, vars.ixmom, vars.iymom, vars.iener, 
+
+    F_x = riemannFunc(1, myg.qx, myg.qy, myg.ng,
+                      vars.nvar, vars.idens, vars.ixmom, vars.iymom, vars.iener,
                       gamma, U_xl, U_xr)
 
-    F_y = riemannFunc(2, myg.qx, myg.qy, myg.ng, 
-                      vars.nvar, vars.idens, vars.ixmom, vars.iymom, vars.iener, 
+    F_y = riemannFunc(2, myg.qx, myg.qy, myg.ng,
+                      vars.nvar, vars.idens, vars.ixmom, vars.iymom, vars.iener,
                       gamma, U_yl, U_yr)
 
     tm_riem.end()
@@ -519,11 +519,8 @@ def unsplitFluxes(my_data, rp, vars, tc, dt):
           (ener[myg.ilo-2:myg.ihi+2,myg.jlo-3:myg.jhi+1] - \
            ener[myg.ilo-2:myg.ihi+2,myg.jlo-2:myg.jhi+2])
 
-    
+
 
     tm_flux.end()
 
     return F_x, F_y
-
-
-
