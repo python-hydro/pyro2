aa8aa2c12253e4907ac0b8fb85b3a8f320289a3a
Michael Zingale <michael.zingale@stonybrook.edu>
2017-12-12 12:43:47 -0500

    implement y direction

--- advection_fv4/interface_states.f90
+++ advection_fv4/interface_states.f90
@@ -35,7 +35,7 @@ subroutine limit(a, qx, qy, ng, idir, &
         do i = ilo-2, ihi+3
 
            ! interpolate to the edges
-           a_int(i,j) = (7.0/12.0)*(a(i-1) + a(i)) - (1.0/12.0)*(a(i-2) + a(i+1))
+           a_int(i,j) = (7.0/12.0)*(a(i-1,j) + a(i,j)) - (1.0/12.0)*(a(i-2,j) + a(i+1,j))
 
            al(i,j) = a_int(i,j)
            ar(i,j) = a_int(i,j)
@@ -115,6 +115,98 @@ subroutine limit(a, qx, qy, ng, idir, &
               endif
               if (abs(dafp(i,j)) >= 2.0d0*abs(dafm(i,j))) then
                  al(i+1,j) = a(i,j) + 2.0d0*dafm(i,j)
+              endif
+           endif
+
+        enddo
+     enddo
+
+  else if (idir == 2) then
+
+     do j = jlo-2, jhi+3
+        do i = ilo, ihi
+
+           ! interpolate to the edges
+           a_int(i,j) = (7.0/12.0)*(a(i,j-1) + a(i,j)) - (1.0/12.0)*(a(i,j-2) + a(i,j+1))
+
+           al(i,j) = a_int(i,j)
+           ar(i,j) = a_int(i,j)
+
+        enddo
+     enddo
+
+     do j = jlo-1, jhi+2
+        do i = ilo, ihi
+           ! these live on cell-centers
+           dafm(i,j) = a(i,j) - a_int(i,j)
+           dafp(i,j) = a_int(i,j+1) - a(i,j)
+
+           ! these live on cell-centers
+           d2af(i,j) = 6.0*(a_int(i,j) - 2.0*a(i,j) + a_int(i,j+1))
+           d2ac(i,j) = a(i,j-1) - 2.0*a(i,j) + a(i,j+1)
+
+           ! this lives on the interface
+           d3a(i,j) = d2ac(i,j) - d2ac(i,j-1)
+
+        enddo
+     enddo
+
+     do j = jlo, jhi+1
+        do i = ilo, ihi
+
+           ! limit? MC Eq. 24 and 25
+           if (dafm(i,j) * dafp(i,j) <= 0.0 .or. &
+                (a(i,j) - a(i,j-2))*(a(i,j+2) - a(i,j)) <= 0.0) then
+
+              ! we are at an extrema
+
+              s = sign(1.0, d2ac(i,j))
+              if (s == sign(1.0, d2ac(i,j-1)) .and. s == sign(1.0, d2ac(i,j+1)) .and. &
+                  s == sign(1.0, d2af(i,j))) then
+                 ! MC Eq. 26
+                 d2a_lim = s*min(abs(d2af(i,j)), C2*abs(d2ac(i,j-1)), &
+                                 C2*abs(d2ac(i,j)), C2*abs(d2ac(i,j+1)))
+              else
+                 d2a_lim = 0.0d0
+              endif
+
+              if (abs(d2af(i,j)) <= 1.e-12*max(abs(a(i,j-2)), abs(a(i,j-1)), &
+                                               abs(a(i,j)), abs(a(i,j+1)), abs(a(i,j+2)))) then
+                 rho = 0.0
+              else
+                 ! MC Eq. 27
+                 rho = d2a_lim/d2af(i,j)
+              enddo
+
+              if (rho < 1.0d0 - 1.d-12) then
+                 ! we may need to limit -- these quantities are at cell-centers
+                 d3a_min = min(d3a(i,j-1), d3a(i,j), d3a(i,j+1), d3a(i,j+2))
+                 d3a_max = max(d3a(i,j-1), d3a(i,j), d3a(i,j+1), d3a(i,j+2))
+
+                 if (C3*max(abs(d3a_min), abs(d3a_max)) <= (d3a_max - d3a_min)) then
+                    ! limit
+                    if (dafm(i,j)*dafp(i,j) < 0.0d0) then
+                       ! Eqs. 29, 30
+                       ar(i,j) = a(i,j) - rho*dafm(i,j)  ! note: typo in Eq 29
+                       al(i,j+1) = a(i,j) + rho*dafp(i,j)
+                    else if (abs(dafm(i,j)) >= 2.0*abs(dafp(i,j))) then
+                       ! Eq. 31
+                       ar(i,j) = a(i,j) - 2.0d0*(1.0d0 - rho)*dafp(i,j) - rho*dafm(i,j)
+                    else if (abs(dafp(i,j)) >= 2.0*abs(dafm(i,j))) then
+                       ! Eq. 32
+                       al(i,j+1) = a(i,j) + 2.0d0*(1.0d0 - rho)*dafm(i,j) + rho*dafp(i,j)
+                    endif
+
+                 endif
+              endif
+
+           else
+              ! if Eqs. 24 or 25 didn't hold we still may need to limit
+              if (abs(dafm(i,j)) >= 2.0d0*abs(dafp(i,j))) then
+                 ar(i,j) = a(i,j) - 2.0d0*dafp(i,j)
+              endif
+              if (abs(dafp(i,j)) >= 2.0d0*abs(dafm(i,j))) then
+                 al(i,j+1) = a(i,j) + 2.0d0*dafm(i,j)
               endif
            endif
 
