e5e8a2f70ce97b7053a8b12c82e468e3b7cc923d
Michael Zingale <michael.zingale@stonybrook.edu>
2017-12-27 14:34:58 -0500

    make this importable without running

--- multigrid/mg_test_vc_constant.py
+++ multigrid/mg_test_vc_constant.py
@@ -40,115 +40,121 @@ def f(x,y):
     return -2.0*((1.0-6.0*x**2)*y**2*(1.0-y**2) + (1.0-6.0*y**2)*x**2*(1.0-x**2))
 
 
-# test the multigrid solver
-nx = 256
-ny = nx
+def test_vc_constant(N):
+
+    # test the multigrid solver
+    nx = N
+    ny = nx
 
 
-# create the coefficient variable -- note we don't want Dirichlet here,
-# because that will try to make alpha = 0 on the interface.  alpha can
-# have different BCs than phi
-g = patch.Grid2d(nx, ny, ng=1)
-d = patch.CellCenterData2d(g)
-bc_c = bnd.BC(xlb="neumann", xrb="neumann",
-              ylb="neumann", yrb="neumann")
-d.register_var("c", bc_c)
-d.create()
+    # create the coefficient variable -- note we don't want Dirichlet here,
+    # because that will try to make alpha = 0 on the interface.  alpha can
+    # have different BCs than phi
+    g = patch.Grid2d(nx, ny, ng=1)
+    d = patch.CellCenterData2d(g)
+    bc_c = bnd.BC(xlb="neumann", xrb="neumann",
+                  ylb="neumann", yrb="neumann")
+    d.register_var("c", bc_c)
+    d.create()
 
-c = d.get_var("c")
-c[:,:] = alpha(g.x2d, g.y2d)
-
-
-plt.clf()
-
-plt.figure(num=1, figsize=(5.0,5.0), dpi=100, facecolor='w')
-
-plt.imshow(np.transpose(c[g.ilo:g.ihi+1,g.jlo:g.jhi+1]),
-          interpolation="nearest", origin="lower",
-          extent=[g.xmin, g.xmax, g.ymin, g.ymax])
-
-plt.xlabel("x")
-plt.ylabel("y")
-plt.title("nx = {}".format(nx))
-
-plt.colorbar()
-
-plt.savefig("mg_alpha.png")
+    c = d.get_var("c")
+    c[:,:] = alpha(g.x2d, g.y2d)
 
 
-# check whether the RHS sums to zero (necessary for periodic data)
-rhs = f(g.x2d, g.y2d)
-print("rhs sum: {}".format(np.sum(rhs[g.ilo:g.ihi+1,g.jlo:g.jhi+1])))
+    plt.clf()
+
+    plt.figure(num=1, figsize=(5.0,5.0), dpi=100, facecolor='w')
+
+    plt.imshow(np.transpose(c[g.ilo:g.ihi+1,g.jlo:g.jhi+1]),
+               interpolation="nearest", origin="lower",
+               extent=[g.xmin, g.xmax, g.ymin, g.ymax])
+
+    plt.xlabel("x")
+    plt.ylabel("y")
+    plt.title("nx = {}".format(nx))
+
+    plt.colorbar()
+
+    plt.savefig("mg_alpha.png")
 
 
-# create the multigrid object
-a = MG.VarCoeffCCMG2d(nx, ny,
-                      xl_BC_type="dirichlet", yl_BC_type="dirichlet",
-                      xr_BC_type="dirichlet", yr_BC_type="dirichlet",
-                      coeffs=c, coeffs_bc=bc_c,
-                      verbose=1)
+    # check whether the RHS sums to zero (necessary for periodic data)
+    rhs = f(g.x2d, g.y2d)
+    print("rhs sum: {}".format(np.sum(rhs[g.ilo:g.ihi+1,g.jlo:g.jhi+1])))
 
 
-# initialize the solution to 0
-a.init_zeros()
-
-# initialize the RHS using the function f
-rhs = f(a.x2d, a.y2d)
-a.init_RHS(rhs)
-
-# solve to a relative tolerance of 1.e-11
-a.solve(rtol=1.e-11)
-
-# alternately, we can just use smoothing by uncommenting the following
-#a.smooth(a.nlevels-1,50000)
-
-# get the solution
-v = a.get_solution()
-
-# compute the error from the analytic solution
-b = true(a.x2d,a.y2d)
-e = v - b
-
-print(" L2 error from true solution = %g\n rel. err from previous cycle = %g\n num. cycles = %d" % \
-      (a.soln_grid.norm(e), a.relative_error, a.num_cycles))
+    # create the multigrid object
+    a = MG.VarCoeffCCMG2d(nx, ny,
+                          xl_BC_type="dirichlet", yl_BC_type="dirichlet",
+                          xr_BC_type="dirichlet", yr_BC_type="dirichlet",
+                          coeffs=c, coeffs_bc=bc_c,
+                          verbose=1)
 
 
-# plot it
-plt.clf()
+    # initialize the solution to 0
+    a.init_zeros()
 
-plt.figure(num=1, figsize=(10.0,5.0), dpi=100, facecolor='w')
+    # initialize the RHS using the function f
+    rhs = f(a.x2d, a.y2d)
+    a.init_RHS(rhs)
 
+    # solve to a relative tolerance of 1.e-11
+    a.solve(rtol=1.e-11)
 
-plt.subplot(121)
+    # alternately, we can just use smoothing by uncommenting the following
+    #a.smooth(a.nlevels-1,50000)
 
-plt.imshow(np.transpose(v[a.ilo:a.ihi+1,a.jlo:a.jhi+1]),
-          interpolation="nearest", origin="lower",
-          extent=[a.xmin, a.xmax, a.ymin, a.ymax])
+    # get the solution
+    v = a.get_solution()
 
-plt.xlabel("x")
-plt.ylabel("y")
-plt.title("nx = {}".format(nx))
+    # compute the error from the analytic solution
+    b = true(a.x2d,a.y2d)
+    e = v - b
 
-plt.colorbar()
-
-
-plt.subplot(122)
-
-plt.imshow(np.transpose(e[a.ilo:a.ihi+1,a.jlo:a.jhi+1]),
-          interpolation="nearest", origin="lower",
-          extent=[a.xmin, a.xmax, a.ymin, a.ymax])
-
-plt.xlabel("x")
-plt.ylabel("y")
-plt.title("error")
-
-plt.colorbar()
-
-plt.tight_layout()
-
-plt.savefig("mg_test.png")
+    print(" L2 error from true solution = %g\n rel. err from previous cycle = %g\n num. cycles = %d" % \
+          (a.soln_grid.norm(e), a.relative_error, a.num_cycles))
 
 
-# store the output for later comparison
-my_data = a.get_solution_object()
-my_data.write("mg_test")
+    # plot it
+    plt.clf()
+
+    plt.figure(num=1, figsize=(10.0,5.0), dpi=100, facecolor='w')
+
+
+    plt.subplot(121)
+
+    plt.imshow(np.transpose(v[a.ilo:a.ihi+1,a.jlo:a.jhi+1]),
+               interpolation="nearest", origin="lower",
+               extent=[a.xmin, a.xmax, a.ymin, a.ymax])
+
+    plt.xlabel("x")
+    plt.ylabel("y")
+    plt.title("nx = {}".format(nx))
+
+    plt.colorbar()
+
+
+    plt.subplot(122)
+
+    plt.imshow(np.transpose(e[a.ilo:a.ihi+1,a.jlo:a.jhi+1]),
+               interpolation="nearest", origin="lower",
+               extent=[a.xmin, a.xmax, a.ymin, a.ymax])
+
+    plt.xlabel("x")
+    plt.ylabel("y")
+    plt.title("error")
+
+    plt.colorbar()
+
+    plt.tight_layout()
+
+    plt.savefig("mg_test.png")
+
+
+    # store the output for later comparison
+    my_data = a.get_solution_object()
+    my_data.write("mg_test")
+
+
+if __name__ == "__main__":
+    test_vc_constant(256)
