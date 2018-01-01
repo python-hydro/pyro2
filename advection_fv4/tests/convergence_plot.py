a0c6a0a20bccf9f6efa5c4a7e53138c906c789a5
Michael Zingale <michael.zingale@stonybrook.edu>
2017-12-31 22:07:08 -0500

    some scaling plots

---
+++ advection_fv4/tests/convergence_plot.py
@@ -0,0 +1,46 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def plot_convergence():
+
+    data4 = np.loadtxt("advection_fv4_convergence.txt")
+
+    nx = data4[:,0]
+    aerr = data4[:,1]
+
+    ax = plt.subplot(111)
+    ax.set_xscale('log')
+    ax.set_yscale('log')
+
+    plt.scatter(nx, aerr, marker="x", color="C1", label="4th-order advection solver")
+    plt.plot(nx, aerr[0]*(nx[0]/nx)**4, "--", color="C0", label=r"$\mathcal{O}(\Delta x^4)$")
+
+    data2 = np.loadtxt("../../advection/tests/advection_convergence.txt")
+
+    nx = data2[:,0]
+    aerr = data2[:,1]
+
+    ax = plt.subplot(111)
+    ax.set_xscale('log')
+    ax.set_yscale('log')
+
+    plt.scatter(nx, aerr, marker="o", color="C1", label="2nd-order advection solver")
+    plt.plot(nx, aerr[0]*(nx[0]/nx)**2, ":", color="C0", label=r"$\mathcal{O}(\Delta x^2)$")
+
+    plt.xlabel("number of zones")
+    plt.ylabel("L2 norm of abs error")
+
+    plt.legend(frameon=False, fontsize="small")
+
+    plt.title(r"convergence for smooth advection problem", fontsize=11)
+
+    f = plt.gcf()
+    f.set_size_inches(6.0,5.0)
+
+    plt.xlim(8,256)
+
+    plt.savefig("smooth_converge.png", bbox_inches="tight", dpi=150)
+
+
+if __name__== "__main__":
+    plot_convergence()
