97d344f607d53589de734979bf9b4371624b413c
Michael Zingale <michael.zingale@stonybrook.edu>
2018-01-12 19:10:49 -0500

    start of the SDC method

---
+++ compressible_sdc/__init__.py
@@ -0,0 +1,6 @@
+"""This is a 4th order accurate compressible hydrodynamics solver,
+implementing the spatial reconstruction from McCorquodale & Colella
+(2011) but using an SDC scheme for the time integration.
+
+"""
+from .simulation import *
