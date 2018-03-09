9f8ec2499f84aecfabd000c8107418498beb66de
Michael Zingale <michael.zingale@stonybrook.edu>
2018-03-08 20:59:30 -0500

    flake8

--- advection_weno/__init__.py
+++ advection_weno/__init__.py
@@ -1,7 +1,8 @@
-"""
-The pyro advection solver.  This implements a finite difference Lax-Friedrichs
-flux split method with WENO reconstruction based on Shu's review from 1998 (https://ntrs.nasa.gov/archive/nasa/casi.ntrs.nasa.gov/19980007543.pdf) although
-the notation more follows Gerolymos et al
+"""The pyro advection solver.  This implements a finite difference
+Lax-Friedrichs flux split method with WENO reconstruction based on
+Shu's review from 1998
+(https://ntrs.nasa.gov/archive/nasa/casi.ntrs.nasa.gov/19980007543.pdf)
+although the notation more follows Gerolymos et al
 (https://doi.org/10.1016/j.jcp.2009.07.039).
 
 Most of the code is taken from advection_rk and toy-conslaw.
