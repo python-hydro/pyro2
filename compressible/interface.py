97e7cc8516dce21ca4a1cfe44b65e44483a34121
Alice Harpole <aliceharpole@gmail.com>
2018-10-31 11:40:38 -0400

    Fixed flake8 complaints

--- compressible/interface.py
+++ compressible/interface.py
@@ -1110,7 +1110,7 @@ def consFlux(idir, gamma, idens, ixmom, iymom, iener, irhoX, nvar, nspec, U_stat
     u = U_state[ixmom] / U_state[idens]
     v = U_state[iymom] / U_state[idens]
 
-    p = (U_state[iener] - 0.5 * U_state[idens]* (u * u + v * v)) * (gamma - 1.0)
+    p = (U_state[iener] - 0.5 * U_state[idens] * (u * u + v * v)) * (gamma - 1.0)
 
     if (idir == 1):
         F[idens] = U_state[idens] * u
