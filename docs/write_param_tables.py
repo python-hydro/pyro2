60b01b8a1a4ca18735a474cbe0d35d6ed6374553
Michael Zingale <michael.zingale@stonybrook.edu>
2019-01-03 12:18:07 -0500

    script to write sphinx tables of parameters

---
+++ docs/write_param_tables.py
@@ -0,0 +1,21 @@
+import os
+import glob
+
+from util import runparams
+
+pfiles = []
+for path, dirs, files in os.walk("../"):
+    for d in dirs:
+        for f in glob.iglob(os.path.join(path, d, "_defaults")):
+            pfiles.append(f)
+
+for f in pfiles:
+    rp = runparams.RuntimeParameters()
+    rp.load_params(f)
+
+    pre, name = os.path.split(f)
+    outfile = "source/{}.{}.inc".format(pre.replace("../", ""), name)
+
+    rp.print_sphinx_tables(outfile=outfile.format(os.path.basename(f)))
+
+
