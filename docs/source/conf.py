0b28913913523072b727f17035b7bac404ee4f36
Michael Zingale <michael.zingale@stonybrook.edu>
2022-10-11 14:31:31 -0400

    add a doc build github action

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -51,6 +51,9 @@ bibtex_bibfiles = ["refs.bib"]
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
+# always execute notebooks
+nbsphinx_execute = 'always'
+
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
 #
@@ -65,7 +68,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'pyro'
-copyright = '2018, pyro development team'
+copyright = '2022, pyro development team'
 author = 'pyro development team'
 
 # The version info for the project you're documenting, acts as replacement for
