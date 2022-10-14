0e5a9d74bf1c77c218595c6f1d9f1be7bb23b026
Michael Zingale <michael.zingale@stonybrook.edu>
2022-10-14 14:18:41 -0400

    copy the defaults files

--- setup.py
+++ setup.py
@@ -1,4 +1,28 @@
+from pathlib import Path
+
 from setuptools import setup, find_packages
+
+# find all of the "_default" files
+defaults = []
+for path in Path("pyro").rglob("_default*"):
+    defaults.append(str(path.relative_to("./pyro")))
+print(defaults)
+
+benchmarks = ["advection/tests/*.h5",
+              "advection_fv4/tests/*.h5",
+              "advection_nonuniform/tests/*.h5",
+              "advection_rk/tests/*.h5",
+              "compressible/tests/*.h5",
+              "compressible/tests/*.h5",
+              "compressible/tests/*.h5",
+              "compressible_fv4/tests/*.h5",
+              "compressible_rk/tests/*.h5",
+              "compressible_sdc/tests/*.h5",
+              "diffusion/tests/*.h5",
+              "incompressible/tests/*.h5",
+              "lm_atm/tests/*.h5",
+              "multigrid/tests/*.h5",
+              "swe/tests/*.h5"]
 
 setup(name='pyro',
       description='A python hydrodynamics code for teaching and prototyping',
@@ -6,21 +30,7 @@ setup(name='pyro',
       license='BSD',
       packages=find_packages(),
       scripts=["pyro/pyro_sim.py"],
-      package_data={"pyro": ["advection/tests/*.h5",
-                             "advection_fv4/tests/*.h5",
-                             "advection_nonuniform/tests/*.h5",
-                             "advection_rk/tests/*.h5",
-                             "compressible/tests/*.h5",
-                             "compressible/tests/*.h5",
-                             "compressible/tests/*.h5",
-                             "compressible_fv4/tests/*.h5",
-                             "compressible_rk/tests/*.h5",
-                             "compressible_sdc/tests/*.h5",
-                             "diffusion/tests/*.h5",
-                             "incompressible/tests/*.h5",
-                             "lm_atm/tests/*.h5",
-                             "multigrid/tests/*.h5",
-                             "swe/tests/*.h5"]},
+      package_data={"pyro": benchmarks + defaults},
       install_requires=['numpy', 'numba', 'matplotlib', 'h5py'],
       use_scm_version={"version_scheme": "post-release",
                        "write_to": "pyro/_version.py"},
