60566287462c1b5384ee7ade462177b11376e1c6
Michael Zingale <michael.zingale@stonybrook.edu>
2022-10-21 15:20:19 -0400

    simplify overrides

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -122,11 +122,7 @@ html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
-html_context = {
-    'css_files': [
-        '_static/theme_overrides.css',  # override wide tables in RTD theme
-        ],
-    }
+html_css_files = ["theme_overrides.css"]
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
