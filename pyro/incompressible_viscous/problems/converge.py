290446ba7d4ccccd576fa9a8b5b6a74fbb8f90a2
Simon Guichandut <simonguichandut@gmail.com>
2023-04-04 15:52:43 -0400

    add incompressible_viscous converge problem

--- pyro/incompressible_viscous/problems/converge.py
+++ pyro/incompressible_viscous/problems/converge.py
@@ -1,29 +1,25 @@
 r"""
-Initialize a smooth incompressible convergence test.  Here, the
+Initialize a smooth incompressible+viscous convergence test.  Here, the
 velocities are initialized as
 
 .. math::
 
-    u(x,y) = -\cos(2 \pi x)\sin(2 \pi y)
+    u(x,y) = 1 - 2 \cos(2 \pi x) \sin(2 \pi y)
 
-    v(x,y) =  \sin(2 \pi x)\cos(2 \pi y)
+    v(x,y) = 1 + 2 \sin(2 \pi x) \cos(2 \pi y)
 
-and the exact solution at some later time t is then
+and the exact solution at some later time t, for some viscosity nu, is
 
 .. math::
 
-    u(x,y,t) = -\cos(2 \pi x)\sin(2 \pi y) e^{-8 \pi^2 \nu t}
+    u(x,y,t) = 1 - 2 \cos(2 \pi (x - t)) \sin(2 \pi (y - t)) e^{-16 \pi^2 \nu t}
 
-    v(x,y,t) =  \sin(2 \pi x)\cos(2 \pi y) e^{-8 \pi^2 \nu t}
+    v(x,y,t) = 1 + 2 \sin(2 \pi (x - t)) \cos(2 \pi (y - t)) e^{-16 \pi^2 \nu t}
 
-    p(x,y,t) = -1/4 [\cos(4 \pi x) - \cos(4 \pi y)] e^{-16 \pi^2 \nu t})
+    p(x,y,t) = - (\cos(4 \pi (x - t)) + \cos(4 \pi (y - t))) e^{-16 \pi^2 \nu t}
 
 The numerical solution can be compared to the exact solution to
 measure the convergence rate of the algorithm.
-
-This example is taken from "I do like CFD, Vol. 1" by Katate Masatsuka
-Section 7.15.1
-
 """
 
 
@@ -36,9 +32,9 @@ from pyro.util import msg
 
 
 def init_data(my_data, rp):
-    """ initialize the incompressible converge problem """
+    """ initialize the incompressible viscous converge problem """
 
-    msg.bold("initializing the incompressible converge problem...")
+    msg.bold("initializing the incompressible viscous converge problem...")
 
     # make sure that we are passed a valid patch object
     if not isinstance(my_data, patch.CellCenterData2d):
@@ -55,8 +51,8 @@ def init_data(my_data, rp):
         myg.ymin != 0 or myg.ymax != 1):
         msg.fail("ERROR: domain should be a unit square")
 
-    u[:, :] = -np.cos(2.0*math.pi*myg.x2d)*np.sin(2.0*math.pi*myg.y2d)
-    v[:, :] = np.sin(2.0*math.pi*myg.x2d)*np.cos(2.0*math.pi*myg.y2d)
+    u[:, :] = 1.0 - 2.0*np.cos(2.0*math.pi*myg.x2d)*np.sin(2.0*math.pi*myg.y2d)
+    v[:, :] = 1.0 + 2.0*np.sin(2.0*math.pi*myg.x2d)*np.cos(2.0*math.pi*myg.y2d)
 
 
 def finalize():
