5a02473576b3642f20aac1c55da0b3d450f92231
Michael Zingale <michael.zingale@stonybrook.edu>
2023-07-04 10:38:52 -0400

    some updates to the plotvar script we can now set dpi ghost cells are filled and vort is an allowed variable

--- pyro/analysis/plotvar.py
+++ pyro/analysis/plotvar.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python3
+"""
+plot a single variable from an output file
+
+Usage: ./plotvar.py filename variable
+"""
 
 import argparse
 
@@ -8,16 +13,13 @@ import numpy as np
 import pyro.util.io_pyro as io
 from pyro.mesh import patch
 
-# plot a single variable from an output file
-#
-# Usage: ./plotvar.py filename variable
-
 
 def makeplot(plotfile, variable, outfile,
-             width=6.5, height=5.25,
+             width=6.5, height=5.25, dpi=100,
              log=False, compact=False, quiet=False):
 
     sim = io.read(plotfile)
+    sim.cc_data.fill_BC_all()
 
     if isinstance(sim, patch.CellCenterData2d):
         myd = sim
@@ -27,7 +29,20 @@ def makeplot(plotfile, variable, outfile,
 
     plt.figure(num=1, figsize=(width, height), dpi=100, facecolor='w')
 
-    var = myd.get_var(variable)
+    if variable == "vort":
+        vx = myd.get_var("x-velocity")
+        vy = myd.get_var("y-velocity")
+
+        var = myg.scratch_array()
+
+        var[myg.ilo:myg.ihi+1, myg.jlo:myg.jhi+1] = \
+             0.5*(vy[myg.ilo+1:myg.ihi+2, myg.jlo:myg.jhi+1] -
+                  vy[myg.ilo-1:myg.ihi, myg.jlo:myg.jhi+1])/myg.dx - \
+             0.5*(vx[myg.ilo:myg.ihi+1, myg.jlo+1:myg.jhi+2] -
+                  vx[myg.ilo:myg.ihi+1, myg.jlo-1:myg.jhi])/myg.dy
+
+    else:
+        var = myd.get_var(variable)
 
     if log:
         var = np.log10(var)
@@ -47,7 +62,7 @@ def makeplot(plotfile, variable, outfile,
         plt.subplots_adjust(bottom=0.0, top=1.0, left=0.0, right=1.0)
         plt.savefig(outfile)
     else:
-        plt.savefig(outfile, bbox_inches="tight")
+        plt.savefig(outfile, bbox_inches="tight", dpi=dpi)
 
     if not quiet:
         plt.show()
@@ -69,6 +84,8 @@ def get_args():
                         metavar="width", help="plot width (inches)")
     parser.add_argument("-H", type=float, default=5.25,
                         metavar="height", help="plot height (inches)")
+    parser.add_argument("--dpi", type=int, default=100,
+                        metavar="dpi", help="dots per inch")
     parser.add_argument("plotfile", type=str, nargs=1,
                         help="the plotfile you wish to plot")
     parser.add_argument("variable", type=str, nargs=1,
@@ -82,5 +99,5 @@ if __name__ == "__main__":
     args = get_args()
 
     makeplot(args.plotfile[0], args.variable[0], args.o,
-             width=args.W, height=args.H,
+             width=args.W, height=args.H, dpi=args.dpi,
              log=args.log, compact=args.compact, quiet=args.quiet)
