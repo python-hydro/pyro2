570bb2354e1fb10c58f2a7aa78200ce69c55f743
Michael Zingale <michael.zingale@stonybrook.edu>
2024-08-31 20:12:30 -0400

    revert

--- pyro/__init__.py
+++ pyro/__init__.py
@@ -4,8 +4,8 @@ methods.
 
 from ._version import version
 
-
 __version__ = version
+
 
 from pyro.mesh import (BC, ArrayIndexer, ArrayIndexerFC, CellCenterData2d,
                        FaceCenterData2d, FV2d, Grid2d, RKIntegrator)
